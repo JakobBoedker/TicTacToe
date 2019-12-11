288fb8ce8aa9a39dad10d041c21c46f713ab40b5
JakobBoedker <bdkerjakob1@gmail.com>
2019-12-11 21:11:22 +0100

    init cpp

---
+++ TicTacToe.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+void display_board(board){
+
+}
+
+
+void player_input(){
+
+}
+
+
+void place_maker(board, marker, position){
+
+}
+
+
+void win_check(board, markl){
+
+}
+
+
+void choose_first(){
+
+
+}
+
+
+
+void space_check(board, position){
+
+}
+
+
+void full_board_check(board){
+
+}
+
+
+void player_choice(board){
+
+}
+
+
+
+void replay(){
+
+}
+
+
+
+
+
+
+int main(){
+
+}
